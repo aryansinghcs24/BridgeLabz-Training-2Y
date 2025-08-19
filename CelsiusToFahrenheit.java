72f388e2e66e7c6211817d57cc56ea5bc2644b25
aryansinghcs24 <aryan.singh3_cs24@gla.ac.in>
2025-08-19 11:37:59 +0530

    CelsiusToFahrenheit.java

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter temperature in Celsius: ");
+            double celsius = scanner.nextDouble();
+
+            double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+            System.out.println("Fahrenheit: " + fahrenheit);
+        }
+    }
+}
